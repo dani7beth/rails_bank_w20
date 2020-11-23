eb9a2da6b1d2847c9363e558a39c824a6c7e968a
dani7beth <danielledona28@gmail.com>
2020-11-23 12:04:47 -0700

    gixed issue with gems

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,5 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show,:edit,:destroy, :update]
   def index
     @accounts = current_user.accounts
   end
@@ -7,8 +8,28 @@ class AccountsController < ApplicationController
   end
 
   def new
+    @account = Account.new
+  end
+
+  def create
+    #one way to create an account does not sanitize
+    #current)user.accounts.create(name: praams[:account][:name], balance: params[:account][:balance])
+    account current_user.accounts.new(account_params)
+    if account.save
+      redirect_to account_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private 
+  def set_account
+    @account = current_user.accounts.find(params[:id])
+  end
+  def account_params
+    params.require[:account].permit(:name, :balance)
   end
 end
